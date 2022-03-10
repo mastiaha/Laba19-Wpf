96531423a4f4f7b7f121705b8b52736b62b46959
Yovanovich.aa <Yovanovich.aa@DESKTOP-ITANAVJ>
2022-03-10 14:06:06 +0300

    1

---
+++ Laba19 Wpf/Models/DlinCircle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba19_Wpf.Models
+{
+    static class DlinCircle
+    {
+        public static double Add(double r) => Math.Round((2 * Math.PI * r), 2);
+
+    }
+}
---
+++ Laba19 Wpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Laba19_Wpf.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Laba19_Wpf
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double lenght;
+        public double Length
+        {
+            get => lenght;
+            set
+            {
+                lenght = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Length = DlinCircle.Add(Radius);
+        }
+
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (radius != 0)
+                return true;
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new RCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+
+    }
+}
+
